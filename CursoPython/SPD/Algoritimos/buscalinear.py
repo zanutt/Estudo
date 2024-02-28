49cee40362e472f469566462e9606d4416219fdc
FATURAMENTO 5 <faturamento5@santacasa.pv>
2024-02-28 13:40:57 -0300

    featAdd: algoritimo de busca linear

---
+++ CursoPython/SPD/Algoritimos/buscalinear.py
@@ -0,0 +1,13 @@
+array = [10, 50, 30, 70, 80, 20, 90, 40]
+array2 = [10, 70, 60, 80, 35, 90, 30, 65]
+
+KEY = 30
+
+
+
+for i in array:
+    print(i)    
+    if i == KEY:
+        print('achou o 30')
+    else:
+        continue
