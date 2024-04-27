569fe4f50c7cb7f4e87e892eb65275a3118d8c10
zanutt <matszg@live.com>
2024-04-27 17:41:16 -0300

    alguns estudos finalizados e um site apenas front (yodora) montado

---
+++ estudo.js
@@ -0,0 +1,16 @@
+const meuInput = document.getElementById("meu-input")
+const meusParagrafos = document.getElementsByClassName("destaque")
+const meusParagrafosPorTag = document.getElementByTagName("h1")
+
+//Buscando pelo id #
+//const meuElemento = document.querySelector("#meu-input")
+
+//Buscando pela classe 
+//const meuElemento = document.querySelector(".nome-classe")
+
+//Buscando pelo tag name
+//const meuElemento = document.querySelector("nomeTag")
+
+//const meuElemento = document.querySelector("#meu-input")
+const meusElementos = documento.querySelectorAll("p")
+console.log(meusElementos)
